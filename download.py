a22f752b12d3273b9893f007c634d17d2c6f29b9
success-kid <webdozens@gmail.com>
2018-05-14 01:15:14 +0000

    upload module

--- download.py
+++ download.py
@@ -3,13 +3,21 @@ import os
 import platform
 
 if 'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ:
-    if platform.system() == "Windows":
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:\GCP\Project-d23421f021e1.json"
-    else:
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/jeremy455576/GCP/Project-d23421f021e1.json"
+	if platform.system() == "Windows":
+		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:\GCP\Project-d23421f021e1.json"
+	else:
+		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/jeremy455576/GCP/Project-d23421f021e1.json"
         
 storage_client = storage.Client()
 bucket = storage_client.get_bucket("patent-specification")
-filename = "training.tar.gz"
-blob = bucket.get_blob("python_download_description.7z")
-blob.download_to_filename("python_download_description.7z")
+# filename = "python_download_description.7z"
+for f in sys.argv[1:]:
+	filename = sys.argv[0]
+	if filename in os.listdir():
+		print(filename + " exists, please change local filename!")
+	else:
+		blob = bucket.get_blob(filename)
+		blob.download_to_filename(filename)
+
+
+
--- upload.py
+++ upload.py
@@ -1,27 +1,9 @@
-# import boto3
-# import json
-
-# with open("pwd.json", 'r', encoding="utf8") as f:
-#     pwd_data = json.load(f)
-# ACCESS_KEY = pwd_data['ACCESS_KEY']
-# SECRET_KEY = pwd_data['SECRET_KEY']
-# SESSION_TOKEN = pwd_data['SESSION_TOKEN']
-
-# client = boto3.client(
-#     's3',
-#     aws_access_key_id=ACCESS_KEY,
-#     aws_secret_access_key=SECRET_KEY,
-#     aws_session_token=SESSION_TOKEN,
-# )
-
-# client.upload_file('/tmp/hello.txt', 'mybucket', 'hello.txt')
-
-# Imports the Google Cloud client library
-
-
 from google.cloud import storage
 import os
 import platform
+import sys
+
+
 
 if 'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ:
     if platform.system() == "Windows":
@@ -29,19 +11,15 @@ if 'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ:
     else:
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/jeremy455576/GCP/Project-d23421f021e1.json"
         
-storage_client = storage.Client()
+storage_client = storage.Client() 
 bucket = storage_client.get_bucket("patent-specification")
-# print(list(bucket.list_blobs()))
-datadir = os.path.join("training", 'python_download_description')
-dirs = os.listdir(datadir)
-for d in dirs:
-    print(d, "dir start!")
-    files = os.listdir(os.path.join(datadir, d))
-    for f in files:
-        # path for google
-        g_path = os.path.join(datadir, d, f).replace('\\', '/')
-        blob = bucket.blob(g_path)
-        
-        # loacal path
-        path = os.path.join(datadir, d, f)
-        blob.upload_from_filename(path)
+# filename = "training.tar.gz"
+for f in sys.argv[1:]:
+	print(f, "starts")
+	CHUNK_SIZE = 10485760 
+	blob = bucket.blob(f, chunk_size=CHUNK_SIZE)
+	if blob.exists():
+		print("file exists on GCP storage, please change filename!")
+	else:		
+		blob.upload_from_filename(f)
+	
